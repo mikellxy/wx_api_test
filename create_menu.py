356e3f85061598c0583705effff55c39382f2256
李心宇 <lixinyu@lixinyudeMacBook-Pro.local>
2018-12-19 14:41:51 +0800

    add settings

--- create_menu.py
+++ create_menu.py
@@ -1,13 +1,14 @@
 import json
 import requests
 
+from .settings import Config
+
 get_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={appid}&secret={secret}'
 create_menu_url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token={access_token}'
-appid = 'wx67ce505cc57ed363'
-secret = '8fdc163f9f6275688c095b99c92bd6d0'
 
 
-def get_token(appid=appid, secret=secret, url=get_token_url):
+
+def get_token(appid, secret, url=get_token_url):
     url = url.format(appid=appid, secret=secret)
     resp = requests.get(url)
     return resp.json().get('access_token')
@@ -31,7 +32,7 @@ def main():
 
         ]
     }
-    token = get_token()
+    token = get_token(Config.APPID, Config.SECRET)
     ret = update_menu(token, menu_data)
     print(ret)
 
